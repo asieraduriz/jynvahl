bbbdc07298ad68472cb05f59b50b3cb1371a99ba
Asier Aduriz <asier.aduriz@simlocal.com>
2025-04-27 22:40:32 +0200

    setting map

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,95 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart'; // Import flame_tiled
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    GameWidget(
+      game: HexMapGame(),
+      loadingBuilder: (BuildContext context) {
+        return CircularProgressIndicator();
+      },
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HexMapGame extends FlameGame
+    with DragCallbacks, ScrollDetector, ScaleDetector, PanDetector {
+  // Define the path to your TMX file within the assets folder
+  // Adjust the path based on your assets structure in pubspec.yaml
+  static const String mapFileName = 'bg-1.tmx';
+  // If your map is in assets/maps/, and 'assets/maps/' is in pubspec.yaml,
+  // the path flame_tiled needs is just 'hex_map.tmx'.
+  // If only 'assets/' is listed, you might need 'maps/hex_map.tmx'.
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Color backgroundColor() => Colors.white60;
+
+  late World _world;
+  late CameraComponent _camera;
+  late TiledComponent mapComponent;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    super.debugMode = true;
+
+    // Define the desired render size for each tile.
+    // This usually matches the tilewidth/tileheight from your Tiled map.
+    final Vector2 tileRenderSize = Vector2(72.0, 72.0);
+
+    // Load the Tiled map
+    mapComponent = await TiledComponent.load(
+      mapFileName, // The filename relative to asset paths in pubspec.yaml
+      tileRenderSize, // The size each tile should be rendered at
+      prefix: 'assets/tiled/',
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    _world = World(children: [mapComponent]);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    await add(_world);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    _camera = CameraComponent.withFixedResolution(
+      width: 300,
+      height: 200,
+      world: _world,
+    );
+    await add(_camera);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    _camera.moveTo(mapComponent.size * 0.5);
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void onDragUpdate(DragUpdateEvent event) {
+    final Vector2 screenDelta = event.canvasDelta;
+
+    if (_camera.viewfinder.zoom > 0) {
+      // Convert screen delta to world delta by scaling with zoom
+      final worldDelta = screenDelta / _camera.viewfinder.zoom;
+
+      // Move the _camera
+      _camera.viewfinder.position -= worldDelta;
+    }
+
+    super.onDragUpdate(event);
+  }
+
+  double clampZoom(double delta) {
+    return delta.clamp(1, 3.0);
+  }
+
+  late double startZoom;
+
+  @override
+  void onScaleStart(_) {
+    startZoom = _camera.viewfinder.zoom;
+  }
+
+  @override
+  void onScaleUpdate(ScaleUpdateInfo info) {
+    final currentScale = info.scale.global;
+    _camera.viewfinder.zoom = 3;
   }
 }
