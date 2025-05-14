fcce313fef911b3b818877c49fdc0fee2ffb352e
Asier Aduriz <asier.aduriz@simlocal.com>
2025-05-14 08:05:09 +0200

    Made staggered hex generation properly with odd-q, and added sqrt(3) to the max height of the hexagons

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +10,7 @@ class HexagonComponent extends PositionComponent {
   final int id;
   final int row;
   final int column;
+  final Vector2 center;
   final Paint _paint;
   final double hexSize;
 
@@ -25,6 +26,7 @@ class HexagonComponent extends PositionComponent {
     super.position,
     required this.row,
     required this.column,
+    required this.center,
     required this.hexSize,
     required this.id,
     Color color = Colors.blue,
@@ -32,7 +34,7 @@ class HexagonComponent extends PositionComponent {
            Paint()
              ..color = color
              ..style = PaintingStyle.fill,
-       super(size: Vector2(72, 72));
+       super(size: Vector2(hexSize * 2, hexSize * sqrt(3)));
 
   @override
   void render(Canvas canvas) {
@@ -51,165 +53,83 @@ class HexagonComponent extends PositionComponent {
       final y = center.dy + size * sin(angle);
       vertices.add(Offset(x, y));
     }
-
     return vertices;
   }
-
-  // @override
-  // void onTapUp(TapUpEvent event) {
-  //   // TODO: implement onTapUp
-  //   print("Tapped on hexagon at position: $id");
-  // }
 }
 
 class HexagonGame extends FlameGame with TapCallbacks {
   final List<HexagonComponent> _hexes = [];
+  final double hexRadius = 36.0;
+  final int numRows = 3;
+  final int numCols = 5;
+
+  Vector2 _calculateHexPositionOddQ(int row, int column, double hexRadius) {
+    final horizontalSpacing = 1.5 * hexRadius;
+    final verticalSpacing = sqrt(3) * hexRadius;
+    final startX = 50.0;
+    final startY = 50.0;
+
+    double x = startX + column * horizontalSpacing;
+    double y = startY + row * verticalSpacing;
+
+    if (column % 2 == 1) {
+      y += verticalSpacing / 2;
+    }
+
+    return Vector2(x, y);
+  }
+
   @override
   Future<void> onLoad() async {
     debugMode = true;
-    final firstHexPosition = Vector2.all(0);
-    final firstHexagonComponent = HexagonComponent(
-      id: 1,
-      row: 0,
-      column: 0,
-      position: firstHexPosition,
-      hexSize: 36,
-      color: Colors.green,
-    );
 
-    add(firstHexagonComponent);
+    for (int row = 0; row < numRows; row++) {
+      for (int col = 0; col < numCols; col++) {
+        final position = _calculateHexPositionOddQ(row, col, hexRadius);
+        final id = row * numCols + col + 1;
 
-    final secondHexPosition = _calculateNextStaggeredHexagonPositionRight(
-      firstHexPosition,
-      36,
-    );
-
-    print("Second hex position $secondHexPosition");
-
-    final secondHexagonComponent = HexagonComponent(
-      id: 2,
-      row: 0,
-      column: 1,
-      position: secondHexPosition,
-      hexSize: 36,
-      color: Colors.purple,
-    );
-    add(secondHexagonComponent);
-
-    final thirdHexPos = _calculateNextNegativeStaggeredHexagonPositionRight(
-      secondHexPosition,
-      36,
-    );
-    final thirdHexagonComponent = HexagonComponent(
-      id: 3,
-      row: 0,
-      column: 2,
-      position: thirdHexPos,
-      hexSize: 36,
-      color: Colors.red,
-    );
-    add(thirdHexagonComponent);
-
-    final thirdHexOnRow = HexagonComponent(
-      row: 0,
-      column: 2,
-      hexSize: 36,
-      id: 4,
-      position: Vector2.all(0),
-    );
-
-    add(thirdHexOnRow);
-
-    final thirdHexPosition = _calculateNextStaggeredRowHexagonPosition(
-      Vector2.all(0),
-      36,
-    );
-    add(
-      HexagonComponent(
-        id: 3,
-        row: 1,
-        column: 0,
-        position: thirdHexPosition,
-        hexSize: 36,
-        color: Colors.orange,
-      ),
-    );
-
-    final fourthHexPosition = _calculateNextStaggeredHexagonPositionRight(
-      thirdHexPosition,
-      36,
-    );
-
-    final fourthHexagonComponent = HexagonComponent(
-      id: 4,
-      row: 1,
-      column: 1,
-      position: fourthHexPosition,
-      hexSize: 36,
-      color: Colors.yellow,
-    );
-    add(fourthHexagonComponent);
-
-    final fifthHexPosition =
-        _calculateNextNegativeStaggeredHexagonPositionRight(
-          fourthHexPosition,
-          36,
+        final hexagonCenter = Vector2(
+          position.x + hexRadius,
+          position.y + (hexRadius * sqrt(3)),
         );
-    final fifthHexagonComponent = HexagonComponent(
-      id: 5,
-      row: 1,
-      column: 2,
-      position: fifthHexPosition,
-      hexSize: 36,
-      color: Colors.brown,
-    );
-    add(fifthHexagonComponent);
 
-    print("Third hex position $thirdHexPosition");
-    _hexes.addAll([firstHexagonComponent, secondHexagonComponent]);
-  }
+        final hexagonEnd = Vector2(
+          position.x + (2 * hexRadius),
+          position.y + (2 * hexRadius * sqrt(3)),
+        );
+        print("Hexagon position $position $hexagonCenter $hexagonEnd");
 
-  Vector2 _calculateNextStaggeredHexagonPositionRight(
-    Vector2 previousPosition,
-    double hexRadius,
-  ) {
-    final horizontalShift = 1.5 * hexRadius;
-    final verticalShift = sqrt(3) / 2 * hexRadius;
-    return Vector2(
-      previousPosition.x + horizontalShift,
-      previousPosition.y + verticalShift,
-    );
-  }
-
-  Vector2 _calculateNextNegativeStaggeredHexagonPositionRight(
-    Vector2 previousPosition,
-    double hexRadius,
-  ) {
-    final horizontalShift = 1.5 * hexRadius;
-    final verticalShift = sqrt(3) / 2 * -hexRadius;
-    return Vector2(
-      previousPosition.x + horizontalShift,
-      previousPosition.y + verticalShift,
-    );
-  }
-
-  Vector2 _calculateNextStaggeredRowHexagonPosition(
-    Vector2 firstRowHexPosition,
-    double hexRadius,
-  ) {
-    final verticalDistanceBetweenRows = sqrt(3) * hexRadius;
-    return Vector2(
-      firstRowHexPosition.x,
-      firstRowHexPosition.y + verticalDistanceBetweenRows,
-    );
+        final hexagon = HexagonComponent(
+          id: id,
+          row: row,
+          column: col,
+          position: position,
+          center: hexagonCenter,
+          hexSize: hexRadius,
+          color: Color.fromRGBO(
+            row * 50 % 255,
+            col * 50 % 255,
+            (row + col) * 25 % 255,
+            1.0,
+          ),
+        );
+        add(hexagon);
+        _hexes.add(hexagon);
+      }
+    }
   }
 
   @override
   void onTapUp(TapUpEvent event) {
-    // TODO: implement onTapUp
     print("Tapped at ${event.localPosition}");
 
-    final offset = Offset(event.localPosition.x, event.localPosition.y);
+    for (var hex in _hexes) {
+      if (hex.containsPoint(
+        Vector2(event.localPosition.x, event.localPosition.y),
+      )) {
+        print("\nPotential hexagon found ${hex.id}");
+      }
+    }
   }
 }
 
